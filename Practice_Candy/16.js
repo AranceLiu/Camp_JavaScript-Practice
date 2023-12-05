92e7d12c53bbdbf857df898411d1fcd5b2a306d9
AranceLiu <145585863+AranceLiu@users.noreply.github.com>
2023-12-05 21:57:32 +0800

    16_turn_to_camelCase

---
+++ Practice_Candy/16.js
@@ -0,0 +1,15 @@
+// 編號：CANDY-016
+// 程式語言：JavaScript
+// 題目：把原本 snake_case 的字轉換成 camelCase 格式
+// 範例："hello_world" -> "helloWorld"
+
+function toCamelCase(str) {
+  return str
+    .split("_")
+    .map((x, idx) => (idx > 0 ? x.replace(x[0], x[0].toUpperCase()) : x))
+    .join("");
+}
+
+console.log(toCamelCase("book")); // book
+console.log(toCamelCase("book_store")); // bookStore
+console.log(toCamelCase("get_good_score")); // getGoodScore
