08855d903b2b79eae89243213573aaa7e7856fec
AranceLiu <145585863+AranceLiu@users.noreply.github.com>
2023-12-05 21:37:47 +0800

    02-find_lack_of_letter

---
+++ Practice_Candy/02.js
@@ -0,0 +1,22 @@
+// 編號：CANDY-002
+// 程式語言：JavaScript
+// 題目：請寫一小段程式，印出連續陣列裡缺少的字元
+
+const chars1 = ["a", "b", "c", "d", "f", "g"]
+const chars2 = ["O", "Q", "R", "S"]
+
+function missingChar(chars) {
+  const chars_num = chars.map((x) => x.charCodeAt())
+  const conti = []
+  for (let i = 0; i <= chars_num.length; i++) {
+    conti.push(chars_num[0] + i)
+  }
+  const result = conti.filter((x) => chars_num.indexOf(x) == -1)
+  return String.fromCharCode(result)
+}
+
+console.log(missingChar(chars1)) // 印出 e
+console.log(missingChar(chars2)) // 印出 P
+
+// 提示：
+// 可使用字串的 charCodeAt 方法...
